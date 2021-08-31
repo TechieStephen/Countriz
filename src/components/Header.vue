cea7885c0cec71b241e140dee8c1b24005c40f56
ohien stephen <ohienstephen@gmail.com>
2021-08-31 08:35:56 +0100

    initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="header">
+    <h1>Where in the world?</h1>
+    <button type="button">Dark Mode</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
