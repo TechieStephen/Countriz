af4ec5b47ae1396c15dfe28d78dd35bbcf8aa06a
ohien stephen <ohienstephen@gmail.com>
2021-09-27 09:39:51 +0100

    ----------

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-import Detail from '../views/Details.vue'
+import Details from '../views/Details.vue'
 
 const routes = [
   {
@@ -15,7 +15,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: Detail
+    component: Details
   }
 ]
 
