a52f1e106a5c5b02d7eb406ca07bb4b47f960a3c
ohien stephen <ohienstephen@gmail.com>
2021-09-27 09:19:10 +0100

    -------

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,7 @@
        <router-link to="`/details/${item.cca2}`" v-for="item,index in countries" :key="index" class="card">
          <img id="flag" :src="item.flags[0] || item.flags[1]" alt="">
          <ul>
-           <h2>{{item.name.common}}</h2>
+           <h2>{{item.name}}</h2>
            <li><span>Population:</span>{{item.population}}</li>
            <li><span>Region:</span> {{item.region}}</li>
            <li><span>Capital:</span> {{item.capital}}</li>
